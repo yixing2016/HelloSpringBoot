f71d03b8ec585e996bd485d311dfbbab98805e40
sqin <sheng.qin@hotmail.com>
2022-01-16 17:05:27 +0800

    first commit

---
+++ quartz/src/test/java/com/sqin/springboot/quartz/QuartzApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sqin.springboot.quartz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuartzApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
